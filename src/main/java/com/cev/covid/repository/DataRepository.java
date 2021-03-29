13341b0c92aa2cfd0aa7d0575152cf34a14ef7fc
Bruno Piqué <vincemaxie@hotmail.com>
2021-03-29 12:24:06 +0200

    removed application.properties - added @Repository annotations

--- src/main/java/com/cev/covid/repository/DataRepository.java
+++ src/main/java/com/cev/covid/repository/DataRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cev.covid.domain.Data;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DataRepository extends JpaRepository<Data, Long> {
 	List<Data> findByRegion_Name(String name);
 }
--- src/main/java/com/cev/covid/repository/RegionRepository.java
+++ src/main/java/com/cev/covid/repository/RegionRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cev.covid.domain.Region;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
 	Optional<Region> findByName(String name);
 }
